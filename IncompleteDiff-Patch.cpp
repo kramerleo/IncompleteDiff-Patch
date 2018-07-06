4f9a07676601b4a5404554f7192c6176107395fc
kramerleo <40857532+kramerleo@users.noreply.github.com>
2018-07-06 11:44:24 -0400

    Add files via upload

--- IncompleteDiff-Patch.cpp
+++ IncompleteDiff-Patch.cpp
@@ -1,9 +1,77 @@
+//Written by Leo Kramer, 20018
+
+//The Program has a major error, so most of the cout and display is for developing, and trying to figure out the error, (it wouldn't be in the final version).
+//In addition, I was unable to implement the trace and patch additions as there were errors with this base algorithim, and you can't build the program on a shakey base.
+
 #include <fstream> 
 #include <string> 
 #include <iostream>
+#include <array>
 #include <vector>
+#include <bits/stdc++.h>
+
 using namespace std; 
 
+void diff(string s, string s2){
+        //(through array)set maximum possible size of (SES) shortest edit script to the two strings in full added together
+        int n = s.size();
+        int m = s2.size();
+        int max = n + m;
+
+        //convert strings to arrays for efficency
+        char char_array1[n+1];
+        strcpy(char_array1, s.c_str());
+        char char_array2[n+1];
+        strcpy(char_array2, s2.c_str());
+
+        //define some Ints for algorithm.
+        int x=0; //Deletions to first string
+        int y=0; //Additions to first string from second string
+
+        //build array that displays closest x in terms of k
+        int Array[(2 * max + 1)];
+        Array[1]=0;
+        int * p = &Array[0];
+
+        cout << "___________START______________" << endl;
+           //To check 'difference contours' of the shortest edit script using Myers Algorithim (each 'move'):
+            for(int d = 0; ((d <= max) && (d >= 0)); d++ ){
+                //to check different k lines, and how far they reach (to find the one that reaches the end first)
+                //Incremented K to every other line because when d is even, K is even, and when it is odd, it is odd
+                for(int k = -d ; (k <= d); k+=2){
+                    cout << "For K  = " << k << "---------" << endl;
+                    if ((k == -d)||((k != d) && (Array[k-1] < Array[k+1]))){cout << "K-1 Array: " << Array[k-1] << " <-> K+1 Array: " << Array[k+1] << endl;
+                        x = (Array[k+1]); cout << "[Past Function: X=Arrayk+1] X= " << x << endl;
+                    } else {
+                        x = (Array[k-1]+1); cout << "[Past Function: X=Array[k+1]+1] X=" << x << endl;}
+                    y = x-k;
+                    //for when both chars are the same (can skip)
+                    if ((x < n) && (y < m) && (char_array1[x+1] == char_array2[y+1])) {cout << "Snake" << endl;
+                        x = x+1;cout << "[Past Function: X=x+1] X=" << x << endl; 
+                        y = y+1;cout << "[Past Function: y=y+1] Y=" << y << endl;
+                    } 
+                    Array[k] = x;
+                    //For Solution:
+                    if ((x>=n) && (y >= m)) {cout << "Found D! For N: " << n << ", M: " << m << ", X: " << x << ", and Y: " << y << endl;
+                        } 
+                }
+                cout << "________End of D:" << d << "__________" << endl;
+                //Wait for Input to move to next D Contour
+                cin.get();
+            
+        //Suspect main error to be accessing negative index of arrays, but im not sure how to solve.
+
+         //MISSING: A TRACE PROGRAM TO CREATE DIFF FILE:
+            //store a copy of each ste
+            //create new file with these stored values
+
+        //MISSING: PATCH PROGRAM:
+            //input patch file, store values
+            //subtract the negatives, add the positives
+            
+            } 
+}
+        
 int main(int argc, char** argv) {
         //convert file 1 into a string
         const char* filename = argv[1];
@@ -15,19 +83,7 @@ int main(int argc, char** argv) {
         ifstream text2(filename2);
         string s2;
         text2 >> s2;
-        //(through array)set maximum possible size of (SES) shortest edit script to the two strings in full added together
-        int n = s.size();
-        int m = s2.size();
-        int max = n + m;
-        //define some Ints for algorithm.
-        int d; //depth, amount of 'moves' made in standard graph
-        int k; //k=x-y
-        int x; //Deletions to first string
-        int y; //Additions to first string from second string
-        //to build array that displays closest x in terms of k
-        int Array[2 * max + 1];
-        Array[0] = 0; 
-
+        
         //If there are no files input.
         if (argc == 1) { cout << "Please input two files." << endl;}
 
@@ -36,35 +92,7 @@ int main(int argc, char** argv) {
 
         //If two files are input, plus a display of contents/name to check:
         else if (argc == 3) { cout << filename << " : " << s << endl; cout << filename2 << " : " << s2 << endl; 
-            //To check about the shortest edit script using Myers Algorithim:
-            if((d <= max) && (d >= 0)){ d++;
-                cout << max << "D:" << d << endl;
-                if((k < d) && (k > -d)){k+=2;
-                    cout << "K:" << k << endl;
-                    if ((k == -d)||((k != d) && (Array[k-1] < Array[k+1]))){
-                        x = (Array[k+1]); cout << Array[k] << " x2:" << x << endl;
-                        y = x-k;
-                    } else {x = (Array[k-1]+1);}
-                    while ((x < n) && (y < m) && (s[x] == s2[y])) {
-                        x = x+1; cout << "x3:" << x << endl;
-                        y = y+1; cout << "y3:" << y << endl;
-                    } 
-                    Array[k] = x;
-                    if ((x>=n) && (y >= m)) {
-                        cout << "D:" << d << " K:" << k << " X:" << x << " Y:" << y << endl;}
-                } else { cout << "Problem with K" << endl;}
-            } else {cout << "Problem with D" << endl;}   
-//HAVING A PROBLEM HERE (I think its with the array)
-        
-        //A TRACE PROGRAM TO CREATE DIFF FILE:
-            //store a copy of each ste
-            //create new file with these stored values
-
-        //PATCH PROGRAM:
-            //input patch file, store values
-            //subtract the negatives, add the positives
-
-        }              
+        diff(s,s2); }
 
         //If more than 2 files are input.
         else if (argc >= 4) { cout << "Can only use two files." << endl;}
